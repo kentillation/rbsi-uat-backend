b7433ef6dba5cb23ee4cc57cbf0be32f5c35e620
Kent Anthony Engbino <kentanthony070495>
2024-11-28 10:54:10 +0800

    Updated

--- app/Http/Controllers/ClientInfoController.php
+++ app/Http/Controllers/ClientInfoController.php
@@ -418,22 +418,22 @@ class ClientInfoController extends Controller
         }
     }
 
-    public function updateClient(Request $request, $cid)
+    public function updateClient(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'type' => 'required|integer',
-            'title' => 'required|integer',
-            'client_status' => 'required|integer',
+            'type' => 'required|string',
+            'title' => 'required|string',
+            'client_status' => 'required|string',
             'first_name' => 'required|string',
             'middle_name' => 'required|string',
             'last_name' => 'required|string',
             'suffix' => 'nullable|string',
             'initial' => 'nullable|string',
             'display_name' => 'required|string',
-            'staff_or_not' => 'required|integer',
+            'staff_or_not' => 'required|integer|in:0,1',
             'tin' => 'nullable|string',
-            'gender' => 'required|integer',
-            'civil_status' => 'required|integer',
+            'gender' => 'required|string',
+            'civil_status' => 'required|string',
             'birthdate' => 'required|string',
             'mobile1' => 'required|string',
             'mobile2' => 'nullable|string',
@@ -444,77 +444,215 @@ class ClientInfoController extends Controller
             'address_line3' => 'required|string',
             'address_line4' => 'nullable|string',
             'postal_code' => 'required|string',
-            'address_type' => 'required|integer',
+            'address_type' => 'required|string',
             'telephone' => 'nullable|string',
             'fax' => 'nullable|string',
             'institution' => 'required|string',
             'entity' => 'required|string',
             'employment' => 'required|string',
-            'tax_code' => 'required|integer',
+            'image_file' => 'required|image|mimes:jpg,png,jpeg|max:2048',
+            'tax_code' => 'nullable|string',
         ]);
-
         if ($validator->fails()) {
             return response()->json(['message' => $validator->errors()->first()], 422);
         }
-
-        // // Check if user exists in SQL Server
+        $filePath = null;
+        if ($request->hasFile('image_file')) {
+            $image = $request->file('image_file');
+            $test = '';
+            $filename = time() . '.' . $image->getClientOriginalExtension();
+            $filePath = $image->storeAs($test, $filename);
+        }
         $existingSqlsrvClient = MBWinClientInfoModel::where('Name1', $request->input('first_name'))
             ->where('Name2', $request->input('middle_name'))
             ->where('Name3', $request->input('last_name'))
             ->first();
-
-        // // Check if Client exists in MySQL
         $existingMysqlClient = ClientInfoModel::where('first_name', $request->input('first_name'))
             ->where('middle_name', $request->input('middle_name'))
             ->where('last_name', $request->input('last_name'))
-            ->where('gender', $request->input('gender'))
-            ->where('birthdate', $request->input('birthdate'))
-            ->where('staff_or_not', $request->input('staff_or_not'))
             ->first();
-
         if ($existingSqlsrvClient || $existingMysqlClient) {
             return response()->json(['message' => 'Client already exists.'], 409);
         }
-
-        // Find existing client
-        $client = ClientInfoModel::where('cid', $cid)->first();
-        if (!$client) {
-            return response()->json(['message' => 'Client not found.'], 404);
+        $typeId = $request->input('type');
+        $titleId = $request->input('title');
+        $client_statusId = $request->input('client_status');
+        $suffixesId = $request->input('suffix');
+        $genderId = $request->input('gender');
+        $civil_statusId = $request->input('civil_status');
+        $address_typeId = $request->input('address_type');
+        $institutionId = $request->input('institution');
+        $entityId = $request->input('entity');
+        $employmentId = $request->input('employment');
+        $type = TypesModel::where('id', $typeId)->first();
+        $title = TitlesModel::where('id', $titleId)->first();
+        $sufFix = SuffixesModel::where('id', $suffixesId)->first();
+        $client_status = ClientStatusModel::where('id', $client_statusId)->first();
+        $gender = GendersModel::where('id', $genderId)->first();
+        $civil_status = CivilStatusModel::where('id', $civil_statusId)->first();
+        $address_type = AddressTypeModel::where('id', $address_typeId)->first();
+        $institution = InstitutionModel::where('id', $institutionId)->first();
+        $entity = EntityModel::where('id', $entityId)->first();
+        $employment = EmploymentModel::where('id', $employmentId)->first();
+        $staffValue = $request->input('staff_or_not');
+        if ($staffValue == 1) {
+            $staffValue = 'T';
+        }
+        if ($staffValue == 2) {
+            $staffValue = 'F';
+        }
+        if (!$type) {
+            return response()->json(['message' => 'Invalid type value.'], 422);
+        }
+        if (!$title) {
+            return response()->json(['message' => 'Invalid title value.'], 422);
+        }
+        if (!$client_status) {
+            return response()->json(['message' => 'Invalid client status value.'], 422);
+        }
+        if (!$sufFix) {
+            return response()->json(['message' => 'Invalid suffix value.'], 422);
+        }
+        if (!$gender) {
+            return response()->json(['message' => 'Invalid gender value.'], 422);
+        }
+        if (!$civil_status) {
+            return response()->json(['message' => 'Invalid civil status value.'], 422);
+        }
+        if (!$address_type) {
+            return response()->json(['message' => 'Invalid address type value.'], 422);
+        }
+        if (!$institution) {
+            return response()->json(['message' => 'Invalid institution value.'], 422);
+        }
+        if (!$entity) {
+            return response()->json(['message' => 'Invalid entity value.'], 422);
+        }
+        if (!$employment) {
+            return response()->json(['message' => 'Invalid employment value.'], 422);
         }
 
-        // Update client details
-        DB::transaction(function () use ($request, $client) {
-            $client->update([
-                'type' => $request->input('type'),
-                'title' => $request->input('title'),
-                'client_status' => $request->input('client_status'),
-                'first_name' => $request->input('first_name'),
-                'middle_name' => $request->input('middle_name'),
-                'last_name' => $request->input('last_name'),
-                'initial' => $request->input('initial'),
-                'display_name' => $request->input('display_name'),
-                'staff_or_not' => $request->input('staff_or_not'),
-                'tin' => $request->input('tin'),
-                'gender' => $request->input('gender'),
-                'civil_status' => $request->input('civil_status'),
-                'birthdate' => $request->input('birthdate'),
-                'mobile1' => $request->input('mobile1'),
-                'mobile2' => $request->input('mobile2'),
-                'email' => $request->input('email'),
-                'nationality' => $request->input('nationality'),
-                'address_line1' => $request->input('address_line1'),
-                'address_line2' => $request->input('address_line2'),
-                'address_line3' => $request->input('address_line3'),
-                'postal_code' => $request->input('postal_code'),
-                'address_type' => $request->input('address_type'),
-                'telephone' => $request->input('telephone'),
-                'fax' => $request->input('fax'),
-                'institution' => $request->input('institution'),
-                'entity' => $request->input('entity'),
-                'employment' => $request->input('employment'),
-                'tax_code' => $request->input('tax_code'),
-            ]);
-        });
-        return response()->json(['message' => 'Client has been updated successfully.'], 200);
+        date_default_timezone_set('Asia/Manila');
+        $currentDate = date("Y-m-d");
+        $customerData = $request->all();
+        $customerData = [
+            "messageId" => $request->input('message_id'),
+            "token" => $request->input('token'),
+            "br" => "000000",
+            "cid" => "",
+            "cidType" => $type->type_code, // Individual, Company, Group
+            "title" => $title->title_code,
+            "name1" => $request->input('last_name'),
+            "name2" => $request->input('first_name'),
+            "name3" => $request->input('middle_name'),
+            "name4" => $sufFix->suffix,
+            "displayName" => $request->input('display_name'),
+            "initials" => $request->input('initial'),
+            "mobile1" => $request->input('mobile1'),
+            "mobile2" => $request->input('mobile2'),
+            "email1" => $request->input('email'),
+            "email2" => $request->input('nationality'), //MBWin bug
+            "gender" => $gender->gender_code,
+            "civilStatus" => $civil_status->civil_status_code,
+            "dob" => $request->input('birthdate'),
+            "langType" => "001",
+            //"appType" => "1", //Put it to createAccount
+            //"prType" => "51", //Put it to createAccount
+            //"glCode" => "01", //Put it to createAccount
+            //"ownershipType" => "010", //Put it to createAccount
+            "staff" => $staffValue, // DosriTF in MBWin Database
+            "taxCode" => "001",
+            "address" => [
+                [
+                    "addressType" => $address_type->address_code,
+                    "line1" => "Brgy. " . $request->input('address_line1'),
+                    "line2" => $request->input('address_line2') . " City",
+                    "line3" => $request->input('address_line3'),
+                    "line4" => "Philippines",
+                    "postalCode" => $request->input('postal_code'),
+                    "phone1" => $request->input('telephone'),
+                    "primary" => "T",
+                    "mailing" => "T",
+                    "tempMailing" => "F",
+                    "startDate" => $currentDate // ?
+                ]
+            ],
+            "ccCode1" => $institution->institution_id,
+            "ccCode2" => $entity->entity_id,
+            "ccCode3" => $employment->employment_id,
+            "locationCode" => "OthrR00001",
+        ];
+        $apiUrl = "http://localhost:6500/datasnap/rest/client/createCustomer";
+        $response = Http::withHeaders([
+            'Content-Type' => $this->config['contentType'],
+            'Authorization' => "Basic aWJjbGllbnQ6MTIzNA=="
+        ])->update($apiUrl, $customerData);
+        if ($response->successful()) {
+            $responseData = $response->json();
+            try {
+                $newCID = $responseData['cid'];
+                $newAddr_Recid = MBWinAddressModel::max('Addr_Recid');
+
+                DB::transaction(function () use ($request, $newCID, $filePath) {
+                    ClientInfoModel::update([
+                        'cid' => $newCID,
+                        'type' => $request->input('type'),
+                        'title' => $request->input('title'),
+                        'client_status' => $request->input('client_status'),
+                        'first_name' => $request->input('first_name'),
+                        'middle_name' => $request->input('middle_name'),
+                        'last_name' => $request->input('last_name'),
+                        'suffix' => $request->input('suffix'),
+                        'initial' => $request->input('initial'),
+                        'display_name' => $request->input('display_name'),
+                        'staff_or_not' => $request->input('staff_or_not'),
+                        'tin' => $request->input('tin'),
+                        'gender' => $request->input('gender'),
+                        'civil_status' => $request->input('civil_status'),
+                        'birthdate' => $request->input('birthdate'),
+                        'mobile1' => $request->input('mobile1'),
+                        'mobile2' => $request->input('mobile2'),
+                        'email' => $request->input('email'),
+                        'nationality' => $request->input('nationality'),
+                        'institution' => $request->input('institution'),
+                        'entity' => $request->input('entity'),
+                        'employment' => $request->input('employment'),
+                        'tax_code' => "001",
+                        'image_file' => $filePath,
+                        'branch' => '000000',
+                        'message_id' => $request->input('message_id'),
+                        'token' => $request->input('token'),
+                    ]);
+                });
+                DB::transaction(function () use ($request, $newCID, $newAddr_Recid) {
+                    AddressModel::update([
+                        'cid' => $newCID,
+                        'address_type' => $request->input('address_type'),
+                        'line1' => $request->input('address_line1'),
+                        'line2' => $request->input('address_line2'),
+                        'line3' => $request->input('address_line3'),
+                        'line4' => "Philippines",
+                        'postal_code' => $request->input('postal_code'),
+                        'telephone' => $request->input('telephone'),
+                        'fax' => $request->input('fax'),
+                        'branch' => '000000',
+                        'addr_recid' => $newAddr_Recid,
+                        'message_id' => $request->input('message_id'),
+                        'token' => $request->input('token'),
+                    ]);
+                });
+                return response()->json([
+                    'message' => 'Client has been updated successfully.',
+                    'data' => $responseData
+                ], 200);
+            } catch (\Exception $e) {
+                return response()->json([
+                    'message' => 'Error: ' . $e->getMessage(),
+                    'trace' => $e->getTraceAsString()
+                ], 500);
+            }
+        } else {
+            return response()->json(['message' => 'Failed to create customer', 'error' => $response->json()], $response->status());
+        }
     }
 }
